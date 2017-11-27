539f8c2b76550aab3b8d86ce8ad3e5d0f76c1290
Boyan Hristov <bhristov96@gmail.com>
2017-11-27 23:01:01 +0100

    completed 2. task and added bag

--- src/task05_driving_parallel/scripts/pd_controller_odom.py
+++ src/task05_driving_parallel/scripts/pd_controller_odom.py
@@ -6,12 +6,12 @@ from nav_msgs.msg import Odometry
 from std_msgs.msg import Int16, Float32
 
 experiment_started = False
-desired_y = None
+initial_y = None
 desired_y_delta = 0.2
 initial_time = None
 last_steering_delta = 0
-Kp = 2.5
-Kd = 1
+Kp = 80
+Kd = 600
 dist_axles = 0.3
 lookahead_distance = 0.5
 desired_dist_wall = 0.4
@@ -32,16 +32,17 @@ def odom_callback(odom_msg):
 
     # rospy.loginfo('y: {}'.format(current_y))
     # rospy.loginfo('x: {}'.format(position.x))
-    global initial_time, desired_y, last_steering_delta
+    global initial_time, initial_y, last_steering_delta
 
-    if desired_y is None:
-        desired_y = current_y + desired_y_delta
+    if initial_y is None:
+        initial_y = current_y
 
     calibrated_angle = get_calibrated_steering(90)
-    distance = desired_y - current_y
-    steering_delta = get_delta_heading_from_dis_and_angle(distance, current_yaw, dist_axles,
-                                                          lookahead_distance, desired_dist_wall)
-    rospy.loginfo('steering delta: {}'.format(steering_delta))
+    distance = current_y - initial_y
+    rospy.loginfo('distance: {}'.format(distance))
+    steering_delta = get_delta_heading_from_dis_and_angle(0.5 + distance, current_yaw, dist_axles,
+                                                          lookahead_distance, 0.5 + 0.2)
+    # rospy.loginfo('steering delta: {}'.format(steering_delta))
     steer_angle = Kp * steering_delta + Kd * (steering_delta - last_steering_delta)*10 + calibrated_angle
     last_steering_delta = steering_delta
 
@@ -57,7 +58,7 @@ def odom_callback(odom_msg):
     elif steer_angle > 179:
         steer_angle = 179
 
-    rospy.loginfo('yaw: {}'.format(current_yaw))
+    # rospy.loginfo('yaw: {}'.format(current_yaw))
     pub_steering.publish(steer_angle - current_yaw)
     pub_y_coord.publish(current_y)
 
@@ -72,11 +73,17 @@ def yaw_callback(yaw_msg):
     global current_yaw, initial_yaw
 
     returned_yaw = yaw_msg.data
+    # rospy.loginfo(returned_yaw)
 
     if initial_yaw is None:
         initial_yaw = returned_yaw
 
-    current_yaw = initial_yaw - current_yaw
+    calculated_yaw = returned_yaw - initial_yaw
+    if calculated_yaw < -math.pi:
+        calculated_yaw = 2 * math.pi - calculated_yaw
+
+    if calculated_yaw != 0:
+        current_yaw = calculated_yaw
 
 
 def start_experiment():
