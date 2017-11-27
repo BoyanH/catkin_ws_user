872499c9719a57f9f31fb6b7cb6608f34e0d3a36
Boyan Hristov <bhristov96@gmail.com>
2017-11-27 16:35:35 +0100

    finished code for 2. task, still not tested

--- src/task05_driving_parallel/scripts/pd_controller_odom.py
+++ src/task05_driving_parallel/scripts/pd_controller_odom.py
@@ -9,27 +9,42 @@ from std_msgs.msg import Int16
 # import matplotlib.pyplot as plt
 
 experiment_started = False
-times = []
+desired_y = None
+desired_y_delta = 0.2
+initial_time = None
+last_steering_delta = 0
+Kp = 0.6
+Kd = 0.6
 
 def get_calibrated_steering():
     return 100
 
 
 def odom_callback(odom_msg):
-    steering = Kp * (desired_angle - current_angle) + calibrated_angle
+    pose = odom_msg.pose.pose
+    position = pose.position  # x, y, z
+    current_y = position.y
+    # orientation = pose.orientation  # x, y, z, w
 
+    global initial_time, desired_y, last_steering_delta
 
-    #stop after 10 seconds
-    current_time = rospy.get_time()
-    times.append(current_time)
+    if desired_y is None:
+        desired_y = current_y + desired_y_delta
 
-    if current_time - times[0] >= 13:
+    calibrated_angle = get_calibrated_steering(90)
+    steering_delta = current_y - desired_y
+    steer_angle = Kp * steering_delta + + Kd * (steering_delta - last_steering_delta) + calibrated_angle
+    last_steering_delta = steering_delta
+
+    if initial_time is None:
+        initial_time = rospy.get_time()
+        start_experiment()
+    elif rospy.get_time() - initial_time > 10:
         pub_speed.publish(0)
         rospy.signal_shutdown("done")
 
-
-    rospy.loginfo(steering)
-    pub_steering.publish(steering)
+    pub_steering.publish(steer_angle)
+    pub_y_coord.publish(current_y)
     start_experiment()
 
 
@@ -43,8 +58,6 @@ def start_experiment():
 
     pub_stop_start.publish(0)
     rospy.sleep(1)
-    pub_steering.publish(get_calibrated_steering())
-    rospy.sleep(1)
     pub_speed.publish(-300)
     rospy.sleep(1)
 
@@ -56,7 +69,7 @@ rospy.loginfo('started')
 
 rospy.Subscriber("/odom", Odometry, odom_callback, queue_size=100)
 
-#pub_squared_error = rospy.Publisher("/squared_error", Int16, queue_size=100)
+pub_y_coord = rospy.Publisher("/log/y_coord", Int16, queue_size=100)
 pub_speed = rospy.Publisher("/manual_control/speed", Int16, queue_size=100)
 pub_stop_start = rospy.Publisher("/manual_control/stop_start", Int16, queue_size=100)
 pub_steering = rospy.Publisher("/manual_control/steering", Int16, queue_size=100)
